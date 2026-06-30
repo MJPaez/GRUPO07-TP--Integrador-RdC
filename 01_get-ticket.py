e5bf42f65f6be759d4b5189c7ac02470214fdd38
MJPaez <125938703+MJPaez@users.noreply.github.com>
2026-06-30 04:17:59 -0300

    Update 01_get-ticket.py

--- 01_get-ticket.py
+++ 01_get-ticket.py
@@ -7,7 +7,7 @@ api_url = "http://localhost:58002/api/v1/ticket"
 headers = {
     "content-type": "application/json"
 }
-# ---- AJUSTADO A LA RED DEL GRUPO 07 (username/password) ----
+# ---- AJUSTADO AL USERNAME/ PASSWORD DEL GRUPO 07 ----
 body_json = {
     "username": "admin",
     "password": "cisco123"
